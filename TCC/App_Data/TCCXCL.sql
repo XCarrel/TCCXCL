8f45682de4a66305f653072de65c730bc3a6cf8b
MisterX25 <Xavier.Carrel@cpnv.ch>
2015-01-09 13:22:03 +0100

    Removed isTeamLeadr

--- TCC/App_Data/TCCXCL.sql
+++ TCC/App_Data/TCCXCL.sql
@@ -100,7 +100,6 @@ CREATE TABLE tccmembership(
 	phonenumber varchar(30),
 	email varchar(50),
 	isSiteAdmin bit NOT NULL DEFAULT 0,
-	isTeamLeader bit NOT NULL DEFAULT 0,
 	approved bit NOT NULL DEFAULT 0);
 
 CREATE TABLE NPA(
